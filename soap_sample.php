20bab71030af219bd926118fb9d132a69ca18907
Payamak-Panel <75814092+payamak-panel@users.noreply.github.com>
2023-08-18 12:36:42 +0330

    Create soap_sample.php

---
+++ soap_sample.php
@@ -0,0 +1,12 @@
+<?php
+
+require('lib/Soap_Client.php');
+
+$soapClient = new Pars\Soap_Client('username', 'password');
+
+// print_r($soapClient->GetCredit());
+// echo $soapClient->GetCredit()->GetCreditResult;
+print_r($soapClient->SendSimpleSMS2('09123456789', '5000xxx', 'test sms', false));
+
+
+?>
